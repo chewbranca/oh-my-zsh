40672819458b1e5a3cd76f8c58c8f74d0a39735f
Russell Branca <chewbranca@gmail.com>
2011-11-16 09:23:06 -0800

    Updating aliases and custom includes

---
+++ custom/aliases.zsh
@@ -0,0 +1,10 @@
+# Play dvds in mplayer
+alias mdvd='mplayer dvd://1 -dvd-device /dev/dvd'
+alias tree='tree -C'
+alias clj='java -cp ~/.clojure/clojure.jar clojure.main'
+alias e="emacsclient -c -a '' -n"
+alias et="TERM=xterm-256color emacsclient -t -a ''"
+alias tmux="/usr/local/bin/tmux -2"
+alias tamacs="tmux attach -t emacs"
+alias ta="tmux attach"
+alias pbcopy="xclip -selection clipboard "
