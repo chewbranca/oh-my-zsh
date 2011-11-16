d4fd7d4368eadfb1f0169d9eab5c0afdaf11ad84
Russell Branca <chewbranca@gmail.com>
2011-11-16 09:13:30 -0800

    Monitor commands

--- custom/example.zsh
+++ custom/example.zsh
@@ -3,3 +3,6 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
+alias enable-monitor="xrandr --output LVDS1 --auto --output VGA1 --auto --same-as LVDS1"
+#alias enable-monitor="xrandr --output LVDS1 --off --output VGA1 --auto"
+alias disable-monitor="xrandr --output LVDS1 --auto --output VGA1 --off"
