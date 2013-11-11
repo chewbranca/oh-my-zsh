ae763e8a0a8e52a319eef8887a0271bba1fa05e1
Russell Branca <chewbranca@gmail.com>
2013-11-11 12:18:05 -0800

    Config updates

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -4,13 +4,31 @@ alias tree='tree -C'
 alias clj='java -cp ~/.clojure/clojure.jar clojure.main'
 alias e="emacsclient -c -a '' -n"
 alias et="TERM=xterm-256color emacsclient -t -a ''"
-alias tmux="/usr/bin/tmux -2"
+alias tmux="`which tmux` -2"
 alias tamacs="tmux attach -t emacs"
 alias ta="tmux attach"
-alias pbcopy="xclip -selection clipboard "
+alias less="less -R"
+#alias pbcopy="xclip -selection clipboard "
 alias cds="cd ~/src"
+alias cdc="cd ~/src/cloudant"
+alias cdz="cd ~/src/cloudant/sneezy"
+alias cdn="cd ~/src/nodeverse"
+alias cdb="cd ~/src/couchdb"
+alias cdbf="cd ~/src/couchdb/src/fauxton"
+alias cdbc="cd ~/src/cloudant/dbcore"
+alias phttp="python -m SimpleHTTPServer"
+alias chrome-no-security="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-web-security --incognito --user-data-dir=~/tmp/foo_chrome_user"
+alias canary-no-security="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --disable-web-security --incognito --user-data-dir=~/tmp/foo_canary_user"
+alias couch-conf-op="./configure --prefix=/opt/couchdb --with-js-lib=/usr/local/lib --with-js-include=/usr/local/include"
+alias couch-conf-dev="./configure --with-js-lib=/usr/local/lib --with-js-include=/usr/local/include"
+alias timed-curl='curl -w "@/Users/russellbranca/.misc/curl_format.txt"'
 function pskill() {
 	ps aux | awk "/$1/ { print \$2 }" | xargs kill
+}
+function sshc() {
+	local cluster=$(acurl -s "https://$1.cloudant.com/_membership" | jq '.all_nodes[0]' | sed 's/"dbcore@\(.*\)net"$/\1com/' | xargs echo -n)
+	echo "Connecting to $cluster\n"
+	ssh $cluster
 }
 
 alias x=extract
@@ -18,3 +36,10 @@ alias x=extract
 alias enable-monitor="xrandr --output LVDS1 --auto --output VGA1 --auto --same-as LVDS1"
 #alias enable-monitor="xrandr --output LVDS1 --off --output VGA1 --auto"
 alias disable-monitor="xrandr --output LVDS1 --auto --output VGA1 --off"
+
+#postgres
+alias start_postgres="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
+alias stop_postgres="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
+
+#ctags
+alias erl-ctags="ctags --file-scope=no -R --languages=erlang ."
--- zshrc.zsh
+++ zshrc.zsh
@@ -3,23 +3,35 @@ export ZSH=$HOME/.oh-my-zsh
 
 export EDITOR=vim
 
+export DISABLE_AUTO_UPDATE="true"
+
 #export PATH=$HOME/bin:$PATH
 
-export PATH=$HOME/bin:/opt/emacs-install/emacs/bin:$HOME/.gem/ruby/1.9.1/bin:$PATH
+export NODE_PATH="/usr/local/lib/node_modules"
+export PATH=~/bin/:./node_modules/.bin:/usr/local/sbin:/usr/local/bin:$HOME/bin:/usr/local/share/npm/bin:/opt/emacs-install/emacs/bin:$HOME/.gem/ruby/1.9.1/bin:$PATH
 
 # Virtualenv(wrapper) settings
-export WORKON_HOME=~/Envs
-source /usr/bin/virtualenvwrapper.sh
+#export WORKON_HOME=~/Envs
+#source /usr/bin/virtualenvwrapper.sh
+
+# node.js settings
+export NODE_PATH="/usr/local/lib/node"
+
+# Fix encoding errors on ssh
+export LC_CTYPE="en_US.UTF-8"
 
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
 #export ZSH_THEME="candy"
-export ZSH_THEME="nebirhos"
+#export ZSH_THEME="nebirhos"
+export ZSH_THEME="blinks"
 
 # Load rvm
-[[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm"
+# [[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
+
 # Hack to make sure default rvm is actually used
 [[ -s "/usr/local/rvm/scripts/rvm" ]] && rvm default
 
@@ -49,3 +61,19 @@ source $ZSH/oh-my-zsh.sh
 export NNTPSERVER="news.gmane.org"
 
 #source /home/russell/src/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+
+# Spin up tmux if not already in a tmux session
+# if [ "$TMUX" = "" ]; then tmux; fi
+
+# Load local setting
+[[ -s "$HOME/.zshrc.local" ]] && source "$HOME/.zshrc.local"
+
+# Load erlang
+# [[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm"
+
+# DEFAULT_ERL_VERSION="r15b01"
+# [[ -s "$HOME/erlang/$DEFAULT_ERL_VERSION/activate" ]] && source "$HOME/erlang/$DEFAULT_ERL_VERSION/activate"
+
+export COUCHDB="127.0.0.1:5984"
